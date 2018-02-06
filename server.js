98a4ac72ecd977ce8cb6552fae4edbaad876e71b
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-02-06 22:16:57 +0530

    build process

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express'),
     app = express(),
-    path = require('path')
+    path = require('path');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     return `server is listeniing on port ${port}`;
 });
-app.use(express.static('dist'));
+app.use('/dist', express.static(path.resolve(__dirname, './dist')));
 app.get('/', (request, response) => { 
     return response.sendFile(path.resolve(__dirname, './src/index.html'));
 });
