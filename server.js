54c5818a20efc0f1ba888653ac3cd137747741d0
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-02-24 23:19:38 +0530

    router

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express'),
     path = require('path');
 
 const port = process.env.PORT || 3000;
+const staticFileMiddleware = express.static(path.resolve(__dirname, './dist'));
 app.listen(port, () => {
     console.log(`server is listeniing on port ${port}`);
 });
-app.use('/dist', express.static(path.resolve(__dirname, './dist')));
-app.get('/', (request, response) => { 
+app.use('/dist', staticFileMiddleware);
+app.get('/', (request, response) => {
     return response.sendFile(path.resolve(__dirname, './src/index.html'));
 });
 require('./build/dev-server')(app);
--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Category from './themes/Category.vue';
+import Login from './themes/Login.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+    //mode: 'history', // This is not working for now, see later.
     routes:[{
+        path: '/login',
+        component: Login
+    },
+    {
         path: '/',
         component: Category
     }]
