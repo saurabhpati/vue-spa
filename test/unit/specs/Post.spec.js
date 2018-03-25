ae872eb2878af86b9934b35f876b2bfa13a25863
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-03-25 15:42:42 +0530

    component update test.

--- test/unit/specs/Post.spec.js
+++ test/unit/specs/Post.spec.js
@@ -1,16 +1,34 @@
 import Vue from 'vue';
 import Post from '../../../src/themes/Post.vue';
 
+const _createComponent = () => {
+    let PostConstructor = Vue.extend(Post);
+    let component = new PostConstructor({
+        propsData: {
+            link: 'http:/github.com/saurabhpati'
+        }
+    }).$mount();
+
+    return component;
+}
+
 describe('Post.Vue', () => {
     it('should render the link', () => {
-        const PostConstructor = Vue.extend(Post);
-        let comp = new PostConstructor({
-            propsData: {
-                link: 'http:/github.com/saurabhpati'
-            }
-        }).$mount()
+        let component = _createComponent();
+        expect(component.$el.querySelector('.card-footer-item').getAttribute('href')).to
+            .equal('http:/github.com/saurabhpati');
+    });
 
-        expect(comp.$el.querySelector('.card-footer-item').getAttribute('href')).to
-        .equal('http:/github.com/saurabhpati');
+    it('should update element href when link changes', (done) => {
+        let component = _createComponent();
+        expect(component.$el.querySelector('.card-footer-item').getAttribute('href')).to
+            .equal('http:/github.com/saurabhpati');
+
+        component.link = 'https://twitter.com/saurabhpati';
+        Vue.nextTick(() => {
+            expect(component.$el.querySelector('.card-footer-item').getAttribute('href')).to
+                .equal('https://twitter.com/saurabhpati');
+            done();
+        });
     });
 });
